15ee3d5fab9a193d9142ce75fa982bcdd024614e
rosmapo <190999018+rosmapo@users.noreply.github.com>
2026-06-11 16:12:23 +0000

    Cleaning, debcreate

--- README.md
+++ README.md
@@ -8,6 +8,22 @@ The project is based on the original NotepadNext source code and includes custom
 
 - Linux
 
+## Building from source
+
+### Install dependency
+sudo apt update  
+sudo apt install cmake ninja-build build-essential qt6-base-dev qt6-base-private-dev libxkbcommon-dev
+
+### Clone repo
+git clone --depth=1 https://github.com/rosmapo/mmedit  
+cd mmedit
+
+### Compilation
+mkdir build  
+cd build  
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release  
+cmake --build .  
+
 ## Original Project
 
 NotepadNext:
--- cmake/Packaging.cmake
+++ cmake/Packaging.cmake
@@ -1,11 +1,36 @@
-if(WIN32)
-    include(${CMAKE_CURRENT_LIST_DIR}/PackagingWindows.cmake OPTIONAL)
-endif()
-
-if(APPLE)
-    include(${CMAKE_CURRENT_LIST_DIR}/PackagingMac.cmake OPTIONAL)
-endif()
-
-if(UNIX AND NOT APPLE)
-    include(${CMAKE_CURRENT_LIST_DIR}/PackagingLinux.cmake OPTIONAL)
-endif()
+# cmake/Packaging.cmake
+message(STATUS "Configuring DEB packaging")
+install(TARGETS mmedit
+    RUNTIME DESTINATION bin
+    COMPONENT mmedit
+)
+install(FILES
+    ${CMAKE_SOURCE_DIR}/deploy/linux/mmedit.desktop
+    DESTINATION share/applications
+    COMPONENT mmedit
+)
+install(FILES
+    ${CMAKE_SOURCE_DIR}/icon/mmedit.svg
+    DESTINATION share/icons/hicolor/scalable/apps
+    COMPONENT mmedit
+)
+set(CPACK_COMPONENTS_ALL mmedit)
+set(CPACK_COMPONENT_MMEDIT_DISPLAY_NAME "mmedit")
+set(CPACK_DEB_COMPONENT_INSTALL ON)
+set(CPACK_GENERATOR "DEB")
+set(CPACK_PACKAGE_VERSION ${PROJECT_VERSION})
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Build <build@local>")
+set(CPACK_DEBIAN_PACKAGE_SECTION "editors")
+set(CPACK_DEBIAN_PACKAGE_HOMEPAGE "https://github.com/rosmapo/mmedit")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Personal reimplementation of Notepad++")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS
+    "libqt6core6 (>= 6.2) | libqt6core6t64 (>= 6.2), \
+     libqt6gui6 (>= 6.2) | libqt6gui6t64 (>= 6.2), \
+     libqt6widgets6 (>= 6.2) | libqt6widgets6t64 (>= 6.2), \
+     libqt6network6 (>= 6.2) | libqt6network6t64 (>= 6.2), \
+     libqt6printsupport6 (>= 6.2) | libqt6printsupport6t64 (>= 6.2), \
+     libqt6core5compat6 (>= 6.2), libgl1"
+)
+set(CPACK_DEBIAN_MMEDIT_PACKAGE_FILE_NAME "mmedit-${PROJECT_VERSION}")
+set(CPACK_DEBIAN_MMEDIT_PACKAGE_NAME "mmedit")
+include(CPack)
